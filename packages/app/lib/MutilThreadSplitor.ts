a2963042a80bb2c0f152ca65e806f9531d6e5830
nextc <1391040917@qq.com>
2024-08-13 11:18:55 +0800

    perf: 添加适当的注释

--- packages/app/lib/MutilThreadSplitor.ts
+++ packages/app/lib/MutilThreadSplitor.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from "@wf-upload/utils";
 import { ChunkSplitor } from "./ChunkSplitor";
-import type { Chunk } from './type';
+import type { Chunk } from "./type";
 
 export class MultiThreadSplitor extends ChunkSplitor {
   private workers: Worker[] = new Array(navigator.hardwareConcurrency || 4)
@@ -13,18 +13,30 @@ export class MultiThreadSplitor extends ChunkSplitor {
     );
 
   calcHash(chunks: Chunk[], emitter: EventEmitter<"chunks">): void {
+    // 计算每个 worker 应处理的块数量
     const workerSize = Math.ceil(chunks.length / this.workers.length);
+    // 将块分配给各个 worker
     for (let i = 0; i < this.workers.length; i++) {
       const worker = this.workers[i];
+
+      // 计算当前 worker 处理的块的起始和结束索引
       const start = i * workerSize;
       const end = Math.min((i + 1) * workerSize, chunks.length);
+
+      // 提取当前 worker 要处理的块子集
       const workerChunks = chunks.slice(start, end);
+
+      // 将块发送给 worker 进行处理
       worker.postMessage(workerChunks);
+
+      // 设置回调函数处理 worker 的响应
       worker.onmessage = (e) => {
+        // 通过事件发射器发出处理后的块数据
         emitter.emit("chunks", e.data);
       };
     }
   }
+
   dispose(): void {
     this.workers.forEach((worker) => worker.terminate());
   }
