a230ab4525751aaef1fab5d8f9187f3237d1373b
nextc <1391040917@qq.com>
2024-08-28 16:09:22 +0800

    chore: 完善服务端代码注释

--- packages/app/lib/SplitWorker.ts
+++ packages/app/lib/SplitWorker.ts
@@ -6,7 +6,7 @@ onmessage = function (e) {
   // 如果 chunks 的长度比较大，比如 175 初始计算就会很耗时
   for (const chunk of chunks) {
     calcChunkHash(chunk).then((hash) => {
-      console.log('计算好的 hash 发给主线程', new Date().getTime())
+      // console.log('计算好的 hash 发给主线程', new Date().getTime())
       chunk.hash = hash
       postMessage([chunk])
     })
--- packages/app/lib/chunk.ts
+++ packages/app/lib/chunk.ts
@@ -21,15 +21,28 @@ export function createChunk(
 
 // 计算chunk的hash值
 export function calcChunkHash(chunk: Chunk): Promise<string> {
+  // 函数定义：接受一个Chunk类型的参数，返回一个Promise<string>
+
   return new Promise((resolve) => {
-    // console.log('begin', new Date().getTime())
+    // 创建一个新的Promise，用于异步计算哈希值
+
     const spark = new SparkMD5.ArrayBuffer()
+    // 创建SparkMD5的ArrayBuffer实例，用于计算哈希值
+
     const fileReader = new FileReader()
+    // 创建FileReader实例，用于读取文件内容
+
     fileReader.onload = (e) => {
+      // 定义FileReader的onload事件处理函数
+
       spark.append(e.target?.result as ArrayBuffer)
+      // 将读取的文件内容（ArrayBuffer）添加到spark实例中
+
       resolve(spark.end())
-      // console.log('end', new Date().getTime())
+      // 计算最终的哈希值并通过resolve返回结果
     }
+
     fileReader.readAsArrayBuffer(chunk.blob)
+    // 开始读取chunk的blob内容，读取完成后会触发onload事件
   })
 }
