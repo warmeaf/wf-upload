ab9ee29a0dad3d0ad75f1bde840f06dfa7c03a35
nextc <1391040917@qq.com>
2024-08-28 16:35:39 +0800

    chore: 完善代码注释

--- packages/app/lib/MutilThreadSplitor.ts
+++ packages/app/lib/MutilThreadSplitor.ts
@@ -3,6 +3,7 @@ import { SplitTemplate } from './SplitTemplate'
 import type { Chunk } from './type'
 
 export class MultiThreadSplitor extends SplitTemplate {
+  // 生成 worker 线程实例数组
   private workers: Worker[] = new Array(navigator.hardwareConcurrency || 4)
     .fill(0)
     .map(
@@ -11,16 +12,14 @@ export class MultiThreadSplitor extends SplitTemplate {
           type: 'module',
         })
     )
+  // 是否暂停
   private isPaused: boolean = false
+  // 暂停的块
   private pausedChunks: Chunk[][] = []
 
   calcHash(chunks: Chunk[], emitter: EventEmitter<'chunks'>): void {
-    // console.log('开始计算分片 hash', new Date().getTime())
     // 计算每个 worker 应处理的块数量
     let workerSize = Math.ceil(chunks.length / this.workers.length)
-    // workerSize = workerSize > 30 ? 60 : workerSize
-    // const workerSize = 10
-    console.log(this.workers, workerSize)
     // 将块分配给各个 worker
     for (let i = 0; i < this.workers.length; i++) {
       const worker = this.workers[i]
@@ -34,15 +33,12 @@ export class MultiThreadSplitor extends SplitTemplate {
 
       // 将块发送给 worker 进行处理
       worker.postMessage(workerChunks)
-      console.log('将块发送给 worker 进行处理', new Date().getTime())
 
       // 设置回调函数处理 worker 的响应
       worker.onmessage = (e) => {
         if (this.isPaused) {
-          // If paused, store the chunks without emitting events
           this.pausedChunks.push(e.data)
         } else {
-          // If not paused, emit the chunks event
           emitter.emit('chunks', e.data)
         }
       }
